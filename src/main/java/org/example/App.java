54f5f3a1f7b4f3fda117baa33aa1af0abe62af82
IT22074072 <it22074072@my.sliit.lk>
2024-12-30 16:58:20 +0530

    initial commit

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,18 +1,30 @@
 package org.example;
 
+import org.example.config.AppConfig;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class App
 {
     public static void main( String[] args )
     {
-        ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml"); //in this line created the object
-        Alien obj1 = context.getBean("alien", Alien.class); //in this line just getting the object
-        //obj1.setAge(1);
-        System.out.println(obj1.getAge());
 
-        obj1.code();
+        ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        Desktop dt = context.getBean(Desktop.class);
+        dt.compile();
+
+
+
+
+
+//        ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml"); //in this line created the object
+//        Alien obj1 = context.getBean("alien", Alien.class); //in this line just getting the object
+//        //obj1.setAge(1);
+//        System.out.println(obj1.getAge());
+//
+//        obj1.code();
 
 
 
