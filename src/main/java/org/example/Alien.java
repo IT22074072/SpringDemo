1183079c8f43888db37d22971786beabe3cc38e5
IT22074072 <it22074072@my.sliit.lk>
2024-12-30 11:38:38 +0530

    Autowire

--- src/main/java/org/example/Alien.java
+++ src/main/java/org/example/Alien.java
@@ -6,29 +6,29 @@ public class Alien {
     private Computer com;
 
     public Alien(){
-        System.out.println("Alien object created");
+        //System.out.println("Alien object created");
     }
 
-    public Alien(int age, Computer com){
-        System.out.println("Parameterized Alien constructor called");
-        this.age = age;
-        this.com = com;
-    }
+//    public Alien(int age, Computer com){
+//        System.out.println("Parameterized Alien constructor called");
+//        this.age = age;
+//        this.com = com;
+//    }
 
     public int getAge() {
         return age;
     }
 
     public void setAge(int age) {
-        System.out.println("setter called");
+        //System.out.println("setter called");
         this.age = age;
     }
 
-    public Computer getLap() {
+    public Computer getCom() {
         return com;
     }
 
-    public void setLap(Computer com) {
+    public void setCom(Computer com) {
         this.com = com;
     }
 
