d3f96df184b3c303a7124d9c7395485b0559d139
IT22074072 <it22074072@my.sliit.lk>
2024-12-30 17:09:45 +0530

    Bean name

--- src/main/java/org/example/config/AppConfig.java
+++ src/main/java/org/example/config/AppConfig.java
@@ -7,8 +7,9 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class AppConfig {
 
+//    @Bean(name = {"com2", "jin", "rm"})
     @Bean
-    public Desktop desktop(){
+    public Desktop desktop(){ //default bean name is method name
         return new Desktop();
     }
 
