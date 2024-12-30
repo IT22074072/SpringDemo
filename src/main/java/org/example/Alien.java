0aa53d74f4da1509153ec5a3826fb0f879741236
IT22074072 <it22074072@my.sliit.lk>
2024-12-30 09:18:47 +0530

    setter injection as property value

--- src/main/java/org/example/Alien.java
+++ src/main/java/org/example/Alien.java
@@ -2,10 +2,20 @@ package org.example;
 
 public class Alien {
 
-    int age;
+    private int age;
     public Alien(){
         System.out.println("Alien object created");
     }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        System.out.println("setter called");
+        this.age = age;
+    }
+
     public void code(){
         System.out.println("coding");
     }
--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -8,16 +8,13 @@ public class App
     public static void main( String[] args )
     {
         ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml"); //in this line created the object
-//        Alien obj1 = (Alien) context.getBean("alien"); //in this line just getting the object
-//        obj1.age=1;
-//        System.out.println(obj1.age);
-//
-//        //obj1.code();
-//
-//        Alien obj2 = (Alien) context.getBean("alien"); //in this line just getting the object-even though we get bean 2 times, it will create only one object and using that ref calling code method 2 times
-//        System.out.println(obj2.age);
+        Alien obj1 = (Alien) context.getBean("alien"); //in this line just getting the object
+        //obj1.setAge(1);
+        System.out.println(obj1.getAge());
 
-        //obj2.code();
+        obj1.code();
+
+
 
     }
 }
