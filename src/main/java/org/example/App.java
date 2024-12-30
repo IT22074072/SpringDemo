e1569389d20d204751150d1ff3baf24082c8d9d8
IT22074072 <it22074072@my.sliit.lk>
2024-12-30 08:43:16 +0530

    spring bean xml config

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,10 +1,14 @@
 package org.example;
 
-public class App 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class App
 {
     public static void main( String[] args )
     {
-        Alien obj = new Alien();
+        ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+        Alien obj = (Alien) context.getBean("alien");
         obj.code();
 
     }
